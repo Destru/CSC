88c9ff0643b4e6b558c7b6ff286d0e7a0f7c78fd
Destru Kaneda <alexander@binaerpilot.no>
2017-04-27 02:31:47 -0700

    YSlow

--- index.js
+++
@@ -1,6 +0,0 @@
-var setCurrentYear;
-(setCurrentYear = function() {
-  var copyleft = document.getElementById('copyleft-years');
-  var currentYear = new Date().getFullYear();
-  copyleft.innerHTML = '1982—' + currentYear + '.';
-})();
