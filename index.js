a45f7069bd40f43a6b15bf6f5c7551934916c25a
Destru Kaneda <alexander@binaerpilot.no>
2018-03-03 01:00:56 -0800

    Adds an 'Online Now' count to the CSC homepage. Tightens up the header.
    Discord actually returns correct MIME.

--- index.js
+++ index.js
@@ -50,6 +50,31 @@ var setTwitchLink;
   });
 })();
 
+// get Discord live users
+function checkDiscord(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.open('GET', 'https://discordapp.com/api/guilds/160320676580818951/widget.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+var setDiscordCount;
+(setDiscordCount = function() {
+  checkDiscord(function(response) {
+    var $online = document.getElementById('online');
+    var $count = document.getElementById('discord-count');
+    var json = JSON.parse(response);
+    if (json.members.length > 0) {
+      $online.style.display = 'block';
+      $count.innerHTML = json.members.length;
+    }
+  });
+})();
+
 // set copyright years
 var setCurrentYear;
 (setCurrentYear = function() {
