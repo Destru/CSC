edc22a7e5f6280fdd13324ae46d61972f1bb6190
Destru Kaneda <alexander@binaerpilot.no>
2018-04-30 14:48:53 -0700

    Let's clean this up a bit. These patterns are depreciated.

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // set background
-  var backgrounds = ['18_0312b','18_0313b','18_0314b','18_0315b','18_0316b','18_0317b','18_0318b','18_0319b','18_0323b','18_0325b'];
-  var fewerBackgrounds = ['18_0325b','18_0318b','18_0312b','18_0319b'];
-  var body = document.getElementsByTagName('body')[0];
+function checkDiscord(callback) {
+  const xobj = new XMLHttpRequest();
+  xobj.open('GET', 'https://discordapp.com/api/guilds/160320676580818951/widget.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
 
-  const rand = fewerBackgrounds[Math.floor(Math.random() * fewerBackgrounds.length)];
+document.addEventListener("DOMContentLoaded", function() {
   const app = document.getElementById("app");
   const background = new Image();
+  const backgrounds = ['18_0325b','18_0318b','18_0312b','18_0319b'];
+  const copyleft = document.getElementById('copyleft-years');
+  const currentYear = new Date().getFullYear();
+  const rand = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 
-  background.src = 'images/'+rand+'.jpg';
   app.style.backgroundImage = 'url(images/'+rand+'.jpg)';
-
+  background.src = 'images/'+rand+'.jpg';
   background.onload = function() {
     app.style.opacity = "1";
-  }
-  // discord live users
-  function checkDiscord(callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.open('GET', 'https://discordapp.com/api/guilds/160320676580818951/widget.json', true);
-    xobj.onreadystatechange = function () {
-      if (xobj.readyState == 4 && xobj.status == "200") {
-        callback(xobj.responseText);
+    checkDiscord(function(response) {
+      const online = document.getElementById('online');
+      const onlineCount = document.getElementById('discord-count');
+      const json = JSON.parse(response);
+      if (json.members.length > 0) {
+        onlineCount.innerHTML = json.members.length;
+        online.style.opacity = 1;
       }
-    };
-    xobj.send(null);
+    });
+    copyleft.innerHTML = '2016—' + currentYear + '.';
   }
-  checkDiscord(function(response) {
-    var $online = document.getElementById('online');
-    var $count = document.getElementById('discord-count');
-    var json = JSON.parse(response);
-    if (json.members.length > 0) {
-      $count.innerHTML = json.members.length;
-    }
-    else {
-      $online.innerHTML = 'Online now';
-    }
-    if ($online.style.opacity !== 1) $online.style.opacity = 1;
-  });
-
-  // set copyright years
-  var copyleft = document.getElementById('copyleft-years');
-  var currentYear = new Date().getFullYear();
-  copyleft.innerHTML = '2016—' + currentYear + '.';
 });
